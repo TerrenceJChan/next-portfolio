e5f26a26eed8858903ed6f0d00ead5d33e9ce6cf
Terrence Chan <terrencechan.writer@gmail.com>
2021-06-28 04:38:47 -0400

    Adjust background color

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,29 @@
 import Head from 'next/head';
 
+import Background from '../components/Background';
 import SectionMain from '../components/SectionMain';
 import SectionBio from '../components/SectionBio';
 import SectionProjects from '../components/SectionProjects';
-import Background from '../components/Background';
 import Footer from '../components/Footer';
 
 export default function Home() {
     return (
         <div>
-            <Background />
             <Head>
                 <title>Terrence Chan</title>
                 <meta name="description" content="Welcome to my page and portfolio! I promise to be on best behaviour~" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main>
-                <SectionMain />
-                <SectionBio />
-                <SectionProjects />
-                <Footer />
-            </main>
+
+            <body>
+                <Background />
+                <main>
+                    <SectionMain />
+                    <SectionBio />
+                    <SectionProjects />
+                    <Footer />
+                </main>
+            </body>
         </div>
     );
 }
