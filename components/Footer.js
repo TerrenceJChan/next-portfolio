78e324a152ec7e744cf59494d321b327c3dc5696
Terrence Chan <terrencechan.writer@gmail.com>
2021-06-28 00:27:48 -0400

    Lint semicolon

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 library.add(fab);
 
