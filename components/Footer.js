8e29bcc68458074c990634ccb8c5b0c25b351c55
Terrence Chan <terrencechan.writer@gmail.com>
2021-06-28 00:21:52 -0400

    Add email and envelope favicon to footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,7 @@ import utilStyles from '../styles/utils.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 library.add(fab);
 
@@ -16,7 +17,7 @@ export default function Footer() {
                         © Terrence J Chan 2021
                     </span>
                     <span className={utilStyles.noMargin}>
-                        <a href="https://github.com/TerrenceJChan" rel="nofollow">GitHub&nbsp;<FontAwesomeIcon icon={['fab', 'github']} /></a> / <a href="https://www.linkedin.com/in/terrencejchan/" rel="nofollow">LinkedIn&nbsp;<FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+                        <a href="https://github.com/TerrenceJChan" rel="nofollow">GitHub&nbsp;<FontAwesomeIcon icon={['fab', 'github']} /></a> / <a href="https://www.linkedin.com/in/terrencejchan/" rel="nofollow">LinkedIn&nbsp;<FontAwesomeIcon icon={['fab', 'linkedin']} /></a> / <a href="mailto: contact@terrencejchan.com">Email&nbsp;<FontAwesomeIcon icon={faEnvelope} /></a>
                     </span>
                     {/* <span>
                         Built with Next.JS / Sass
